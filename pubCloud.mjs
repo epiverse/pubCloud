274fbe6bdfe402c5129a069b912c7bc5d194f541
Jonas Almeida <jalmeida@mathbiol.org>
2025-04-02 07:50:30 -0400

    Update pubCloud.mjs

--- pubCloud.mjs
+++ pubCloud.mjs
@@ -3,7 +3,11 @@ console.log('importing from pubCloud.mjs');
 import('https://episphere.github.io/gemini/gem.mjs')
 embed = (new (await import('https://episphere.github.io/gemini/gem.mjs')).GEM).embed
 
-async function embedPMID(pmid=39992653){
+async function embedPMID(pmid=36745477){
     let txt = await (await fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=39992653&retmode=text&rettype=abstract')).text()
     return txt
+}
+
+export{
+    embedPMID
 }
