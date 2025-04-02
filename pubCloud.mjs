0630ab5cc59776aa8c6d79407940063907f90bb1
Jonas Almeida <jalmeida@mathbiol.org>
2025-04-02 11:49:35 -0400

    Update pubCloud.mjs

--- pubCloud.mjs
+++ pubCloud.mjs
@@ -8,7 +8,7 @@ const embed = (new GEM).embed
 async function embedPMID(pmid=36745477){
     // x = await (await import('./pubCloud.mjs')).embedPMID()
     // x = await (await import('https://epiverse.github.io/pubCloud/pubCloud.mjs')).embedPMID()
-    let txt = await (await fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=39992653&retmode=text&rettype=abstract')).text()
+    let txt = await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pmid}&retmode=text&rettype=abstract`)).text()
     let emb = await embed(txt)
     return {
         pmid:pmid,
@@ -17,10 +17,23 @@ async function embedPMID(pmid=36745477){
     }
 }
 
+async function embedPMIDs(pmids=[36745477,39992653,12611807]){
+    let embeds=[]
+    let n = pmids.length
+    console.log(`embedding ...`)
+    for(let i=0; i<n; i++){
+        await embeds.push(await embedPMID(pmids[i]))
+        console.log(`${i+1}/${n}`)
+    }
+    console.log(`embedding ... done`)
+    return embeds
+}
+
 export{
     GEM,
     embed,
-    embedPMID
+    embedPMID,
+    embedPMIDs
 }
 
 // embedPMID = (await import('./pubCloud.mjs')).embedPMID
