18c20c89db34680a752cdacce0d8f2ae18de1dfd
Jonas Almeida <jalmeida@mathbiol.org>
2025-04-04 16:12:17 -0400

    timeout trouble

--- pubCloud.mjs
+++ pubCloud.mjs
@@ -9,7 +9,7 @@ async function embedPMID(pmid=36745477) {
     // x = await (await import('./pubCloud.mjs')).embedPMID()
     // x = await (await import('https://epiverse.github.io/pubCloud/pubCloud.mjs')).embedPMID()
     let txt = await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pmid}&retmode=text&rettype=abstract`)).text()
-    let emb = await embed(txt.slice(0,35314))
+    let emb = await embed(txt.slice(0, 35314))
     return {
         pmid: pmid,
         abstract: txt,
@@ -33,12 +33,13 @@ function saveFile(txt=':-)', fileName="hello.txt") {
     var bb = new Blob([txt]);
     var url = URL.createObjectURL(bb);
     var a = document.createElement('a')
-    a.hidden=true
+    a.hidden = true
     document.body.appendChild(a)
     a.href = url;
     a.download = fileName;
     a.click()
-    a.parentElement.removeChild(a) // cleanup
+    a.parentElement.removeChild(a)
+    // cleanup
     return a
 }
 
@@ -51,7 +52,8 @@ async function readTextFile(fun=console.log) {
         let fr = new FileReader();
         fr.onload = function() {
             fun(fr.result)
-            loadFile.parentElement.removeChild(loadFile) // cleanup
+            loadFile.parentElement.removeChild(loadFile)
+            // cleanup
         }
         fr.readAsText(loadFile.files[0]);
     }
@@ -74,51 +76,70 @@ function tsv2json(tsv){
 }
 */
 
-function tsv2json(tsv){
-    let json = tsv.split(/[\n\r]+/).map(row=>row.split(/\t/))
+function tsv2json(tsv) {
+    let json = tsv.split(/[\n\r]+/).map(row => row.split(/\t/))
     let attrs = json[0]
-    json=json.slice(1).map((row,i)=>{
-        let rowObj={}
-        attrs.forEach((attri,j)=>{
-            rowObj[attri]=row[j]
-        })
+    json = json.slice(1).map( (row, i) => {
+        let rowObj = {}
+        attrs.forEach( (attri, j) => {
+            rowObj[attri] = row[j]
+        }
+        )
         return rowObj
-    })
+    }
+    )
     return json
 }
 
-async function indexPubMedIDs(json){
-    if(!json){
-        json = await assembleFromSource()
+async function indexPubMedIDs(docs) {
+    if (!docs) {
+        docs = await assembleFromSource()
     }
-    let PubMedIDs = json
-        .filter(x=>x.PubMedID)
-        .filter(x=>x.PubMedID.length>0)
-        .map(x=>x.PubMedID)
-        //.slice(20,30) // while debugging
-    let indexedPubMedIDs=await embedPMIDs(PubMedIDs)
+    let PubMedIDs = docs.filter(x => x.PubMedID).filter(x => x.PubMedID.length > 0).map(x => x.PubMedID)
+    //.slice(20,30) // while debugging
+    let indexedPubMedIDs = await embedPMIDs(PubMedIDs)
     return indexedPubMedIDs
 }
-async function listPubMedIDs(json){
-    if(!json){
-        json = await assembleFromSource()
+async function listPubMedIDs(docs) {
+    if (!docs) {
+        docs = await assembleFromSource()
     }
-    let PubMedIDs = json
-        .filter(x=>x.PubMedID)
-        .filter(x=>x.PubMedID.length>0)
-        .map(x=>x.PubMedID)    
+    let PubMedIDs = docs.filter(x => x.PubMedID).filter(x => x.PubMedID.length > 0).map(x => x.PubMedID)
     return PubMedIDs
 }
 
 // Get all absracts from NCBI eutils in batch
-
-async function getAllAbstracts(json){}
+let countBatch=0
+async function getAllAbstracts(docs) {
+    let txts = ''
+    if (!docs) {
+        docs = await assembleFromSource()
+    }
+    const ids = await listPubMedIDs()
+    const k = 250
+    const tLag = 10000
+    // get abstracts in batches of 250
+    for(let i=0 ; i<ids.length ; i=i+k){
+        let batchPubMedIDs = ids.slice(i,i+k)
+        //console.log(batchPubMedIDs)
+        await setTimeout(async function(){
+            countBatch++
+            console.log(`batch #${countBatch}, [n=${batchPubMedIDs.length}] index #${i}`)
+            txts += await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${batchPubMedIDs.join()}&retmode=text&rettype=abstract`)).text()
+        },tLag*countBatch)
+    }
+    return txts
+}
 
 // master assembler
 
-async function assembleFromSource(url='https://raw.githubusercontent.com/epiverse/pubCloud/refs/heads/main/5%20years%20data%20publications.tsv'){
-    let json = await tsv2json(await (await fetch(url)).text())
-    return json
+async function assembleFromSource(url='https://raw.githubusercontent.com/epiverse/pubCloud/refs/heads/main/5%20years%20data%20publications.tsv') {
+    let docs = await tsv2json(await (await fetch(url)).text());
+    docs = docs.map((x,i)=>{
+        x.i=i
+        return x
+    })
+    return docs
 }
 
 export {
@@ -130,8 +151,8 @@ export {
     saveFile,
     assembleFromSource,
     indexPubMedIDs,
-    listPubMedIDs
-}
+    listPubMedIDs,
+    getAllAbstracts}
 
 // embedPMID = (await import('./pubCloud.mjs')).embedPMID
 // embedPMID = (await import('https://epiverse.github.io/pubCloud/pubCloud.mjs')).embedPMID
